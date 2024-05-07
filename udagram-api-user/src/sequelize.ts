e1a054cca4f38a59c9574954b3a875bf8dfed50d
Dat <xuandat28105@gmail.com>
2024-05-07 14:03:28 +0700

    hello again

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,18 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
